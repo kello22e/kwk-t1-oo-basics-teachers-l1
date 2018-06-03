b5b669bbe354e38547f620ac3e4d21d2cd0b17d5
kello22e <kello22e@mtholyoke.edu>
2018-06-03 19:08:38 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,12 +3,19 @@ class Book
   def initialize(title)
     @title = title
   end
+  
   #gets the title
   def title
     @title
   end
+  
   #set author
   def author=(author)
     @author = author
+  end
+  
+  #gets the author
+  def author
+    @author
   end
 end

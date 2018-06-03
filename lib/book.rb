24f3324d5dd2b0291b3b893dfe1fde8cfedde000
kello22e <kello22e@mtholyoke.edu>
2018-06-03 19:13:38 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,39 +7,6 @@ class Book
     @title = title
   end
   
-  #gets the title
-  def title
-    @title
-  end
-  
-  #set author
-  def author=(author)
-    @author = author
-  end
-  
-  #gets the author
-  def author
-    @author
-  end
-  
-  #set page count
-  def page_count=(num)
-    @page_count = num
-  end
-  
-  #get page count
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
- 
-  def genre
-    @genre
-  end
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
